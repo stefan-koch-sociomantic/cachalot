4f562327d9173a77c1ab34d2e392d93a624b8df9
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2017-06-23 12:29:18 +0200

    Add shell utility to wrap the last version tool

---
+++ docker/util.sh
@@ -0,0 +1,25 @@
+# Utility library to use in scripts
+
+apt_ver()
+{
+	pkg="$1"
+	ver="$2"
+
+	# If there is a $ver specified, then get the latest one
+	ver="${ver:+$(./last-version apt "$pkg" "$ver")}"
+
+	# If we still have a version, then pass it to apt explicitly
+	echo "$pkg${ver:+=$ver}"
+}
+
+gem_ver()
+{
+	pkg="$1"
+	ver="$2"
+
+	# If there is a $ver specified, then get the latest one
+	ver="${ver:+$(./last-version gem "$pkg" "$ver")}"
+
+	# If we still have a version, then pass it to apt explicitly
+	echo "${ver:+--version=$ver }$pkg"
+}
